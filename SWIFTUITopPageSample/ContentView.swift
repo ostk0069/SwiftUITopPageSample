d6ec284c3f4c260fea8205bb8c90b3ebfdcef969
Takuma Osada <judgment9ttk069@gmail.com>
2019-06-09 11:22:10 +0900

    show post index by using List

--- SWIFTUITopPageSample/ContentView.swift
+++ SWIFTUITopPageSample/ContentView.swift
@@ -9,14 +9,43 @@
 import SwiftUI
 
 struct ContentView : View {
+    
+    let posts = ["1", "2", "3", "4"]
+    
     var body: some View {
         NavigationView {
             List {
-                Text("first row")
-            }.navigationBarTitle(Text("Hoge"))
+                ForEach(posts.identified(by: \.self)) { post in
+                    PostView()
+                }
+            }.navigationBarTitle(Text("List Title"))
         }
     }
 }
+
+struct PostView: View  {
+    var body: some View {
+        VStack (alignment: .leading, spacing: 12) {
+            HStack {
+                Image("clothes")
+                    .resizable()
+                    .clipShape(Circle())
+                    .frame(width: 60, height: 60)
+                    .clipped()
+                
+                VStack (alignment: .leading, spacing: 4) {
+                    Text("Post View").font(.headline)
+                    Text("Post Date").font(.subheadline)
+                    }.padding(.leading, 8)
+                
+                }.padding(.leading, 16).padding(.top, 16)
+            
+            Text("Post body text htat hopefully support auto sizing vertically and span multiple lines").lineLimit(nil).padding(.leading, 16).padding(.trailing, 32)
+            Image("office")
+            }.padding(.leading, -20)
+    }
+}
+
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
