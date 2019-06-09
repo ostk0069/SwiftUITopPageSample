f01f1b22b191cefb9254e27f4a266ea83e92c1bf
Takuma Osada <judgment9ttk069@gmail.com>
2019-06-09 10:24:25 +0900

    init files

---
+++ SWIFTUITopPageSample/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SWIFTUITopPageSample
+//
+//  Created by Takuma Osada on 2019/06/09.
+//  Copyright © 2019 Takuma Osada. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
